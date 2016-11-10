2726000dbe620a7b7181f8a1bec56e353166bd49
yukicode <yolandaapply@gmail.com>
2016-11-09 23:42:20 -0800

    add detail components

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,9 @@
+//file names in lower dash case (AKA kebab-case) 
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'my-hero-detail',
+})
+export class HeroDetailComponent {
+}
